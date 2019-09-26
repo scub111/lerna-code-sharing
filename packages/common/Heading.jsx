6197541fe30cf36221d3d624c63fe9abc19d1c39
scub111 <scub111@mail.ru>
2019-09-26 15:20:58 +0300

    Work stage.

--- packages/common/Heading.jsx
+++ packages/common/Heading.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 function Heading({ level = "1", title }) {
-  return React.createElement(`h${level}`, {}, title);
+  // return React.createElement(`h${level}`, {}, `title`);
+  return React.createElement(`div`, {}, `title`);
 }
 
 export default Heading;
